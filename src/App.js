a05b38f9b0b5054cb0cf452229d77a66f57bcfe2
Pearce Packman <147752127+pearcepackman@users.noreply.github.com>
2025-04-12 11:04:24 -0400

    4/12 Began Styling

--- src/App.js
+++ src/App.js
@@ -5,18 +5,43 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+        <div className = "navarea">
+          <div className = "topnav">
+            <div className = "birdpic"></div>
+            <div className = "leftnavwords">
+              <h1 className = "biztitle">Early Bird Services, LLC</h1>
+              <h5 className = "bizdesc">Located in Hampstead, MD, serving all of Carroll County and surrounding areas. Lawn , landscape, jun</h5>
+            </div>
+          </div>
+          <div className = "navoptions">
+            
+            <p>Landscaping</p>
+            <p>Snow Removal</p>
+            <p>Demolition</p>
+            <p>Moving Services</p>
+            <p>Contact</p>
+          </div>
+          
+        </div>
+
+        <div className = "workarea">
+          <h1 className = "worktitle">
+            What We Do
+          </h1>
+          <p className = "workdesc">
+            We do all sorts of things from landscaping, snow removal, demolition, moving services, etc.
+          </p>
+          <div className = "cardarea">
+            <div className = "workcard">
+
+            </div>
+            <div className = "workcard">
+
+            </div>
+          </div>
+        </div>
+        
       </header>
     </div>
   );
