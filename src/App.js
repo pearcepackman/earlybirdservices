b389badead212d90cf48879ff76c530d55d648e6
Pearce Packman <147752127+pearcepackman@users.noreply.github.com>
2025-04-13 21:01:44 -0400

    4/13/25

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ function App() {
             What We Do
           </h1>
           <p className = "workdesc">
-            We do all sorts of things from landscaping, snow removal, demolition, moving services, etc.
+            Meow We do all sorts of things from landscaping, snow removal, demolition, moving services, etc.
           </p>
           <div className = "cardarea">
             <div className = "workcard">
