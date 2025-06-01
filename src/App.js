8fe6319d99cf97961339e571e6c5a93bb4163b47
Pearce Packman <pearcepackman@gmail.com>
2025-06-01 15:59:00 -0400

    6/1/2025 - Updated styling and work cards

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import imageplace from './pictures/imageplace.png';
 import './App.css';
 
 function App() {
@@ -11,7 +12,7 @@ function App() {
             <div className = "birdpic"></div>
             <div className = "leftnavwords">
               <h1 className = "biztitle">Early Bird Services, LLC</h1>
-              <h5 className = "bizdesc">Located in Hampstead, MD, serving all of Carroll County and surrounding areas. Lawn , landscape, jun</h5>
+              <h5 className = "bizdesc">Serving all of Carroll County and surrounding areas. Lawn care, landscaping, junk removal, demolition, and snow removal! </h5>
             </div>
           </div>
           <div className = "navoptions">
@@ -30,15 +31,87 @@ function App() {
             What We Do
           </h1>
           <p className = "workdesc">
-            We do all sorts of things from landscaping, snow removal, demolition, moving services, etc.
+            Everything from landscaping, snow removal, demolition, moving, and other services! 
           </p>
           <div className = "cardarea">
             <div className = "workcard">
-
+              <div className = "workcardpic">
+                
+              </div>
+              <div className = "workcardtitle">
+                <p>Landscaping and Lawn Care</p>
+              </div>
+              <div className = "workcarddesc">
+                <p>Some description about the type of work it is and what he does. Im gonna yap about a bunch of stuff here so i can test the view with a description
+                  I feel like if i keep writing this will overflow unfortunately which i dont wanna have to fix lol
+                </p>
+              </div>
             </div>
             <div className = "workcard">
-
+              <div className = "workcardpic">
+                
+              </div>
+              <div className = "workcardtitle">
+                <p>Snow Removal</p>
+              </div>
+              <div className = "workcarddesc">
+                <p>Some description about the type of work it is and what he does. Im gonna yap about a bunch of stuff here so i can test the view with a description
+                  I feel like if i keep writing this will overflow unfortunately which i dont wanna have to fix lol
+                </p>
+              </div>
+            </div><div className = "workcard">
+              <div className = "workcardpic">
+                
+              </div>
+              <div className = "workcardtitle">
+                <p>Demolition Work</p>
+              </div>
+              <div className = "workcarddesc">
+                <p>Some description about the type of work it is and what he does. Im gonna yap about a bunch of stuff here so i can test the view with a description
+                  I feel like if i keep writing this will overflow unfortunately which i dont wanna have to fix lol
+                </p>
+              </div>
             </div>
+            <div className = "workcard">
+              <div className = "workcardpic">
+                
+              </div>
+              <div className = "workcardtitle">
+                <p>Moving Services</p>
+              </div>
+              <div className = "workcarddesc">
+                <p>Some description about the type of work it is and what he does. Im gonna yap about a bunch of stuff here so i can test the view with a description
+                  I feel like if i keep writing this will overflow unfortunately which i dont wanna have to fix lol
+                </p>
+              </div>
+            </div>
+            <div className = "workcard">
+              <div className = "workcardpic">
+                
+              </div>
+              <div className = "workcardtitle">
+                <p>Other Work</p>
+              </div>
+              <div className = "workcarddesc">
+                <p>Some description about the type of work it is and what he does. Im gonna yap about a bunch of stuff here so i can test the view with a description
+                  I feel like if i keep writing this will overflow unfortunately which i dont wanna have to fix lol
+                </p>
+              </div>
+            </div>
+            <div className = "workcard">
+              <div className = "workcardpic">
+                
+              </div>
+              <div className = "workcardtitle">
+                <p>Other Work</p>
+              </div>
+              <div className = "workcarddesc">
+                <p>Some description about the type of work it is and what he does. Im gonna yap about a bunch of stuff here so i can test the view with a description
+                  I feel like if i keep writing this will overflow unfortunately which i dont wanna have to fix lol
+                </p>
+              </div>
+            </div>
+            
           </div>
         </div>
         
