625776d137c8d853f4b6d691535cd1d8050a4b83
Pearce Packman <pearcepackman@gmail.com>
2025-06-06 17:05:21 -0400

    6/6/2025 - Updated styling, contact form and footer

--- src/App.js
+++ src/App.js
@@ -11,7 +11,8 @@ function App() {
           <div className = "topnav">
             <div className = "leftnavwords">
               <h1 className = "biztitle">Early Bird Services LLC</h1>
-              <h5 className = "bizdesc">Serving all of Carroll County and surrounding areas. Lawn care, landscaping, junk removal, demolition, and snow removal! </h5>
+              <h5 className = "bizdesc">Serving all of Carroll County and surrounding areas. Lawn care, landscaping, junk removal, demolition, and snow removal!<br />
+              Commercial and Residential | Licensed and Insured<br />443-536-1255 | earlybirdservicesllcmd@gmail.com<br />Michael Briggs: Owner / Operator</h5>
             </div>
             <div className='righthandnav'>
               <div className = "birdpic"></div>
@@ -116,6 +117,62 @@ function App() {
             </div>
             
           </div>
+        </div>
+        <div className = "linedivider"></div>
+        <div className = "contactarea">
+           <div className = "birdpiccontact"></div>
+          
+          <form className="contact-form">
+            <h2 className = "formtitle">Contact Us</h2>
+            <label>
+              First Name *
+              <input type="text" name="name" required />
+              
+            </label>
+
+            <label>
+              Last Name *
+              <input type="email" name="email" required />
+            </label>
+            <label>
+              Phone *
+              <input type="email" name="email" required />
+            </label>
+            <label>
+              Email *
+              <input type="email" name="email" required />
+            </label>
+            <label>
+              Address *
+              <input type="email" name="email" required />
+            </label>
+            <label>
+              City *
+              <input type="email" name="email" required />
+            </label>
+            <label>
+              State *
+              <input type="email" name="email" required />
+            </label>
+            <label>
+              Zip Code *
+              <input type="email" name="email" required />
+            </label>
+            <label>
+              How Did You Hear About Us?
+              <input type="email" name="email" />
+            </label>
+
+            <label>
+              What Kind of Work is Needed?
+              <textarea name="message" rows="5" required></textarea>
+            </label>
+
+            <button type="submit">Send</button>
+          </form>
+        </div>
+        <div className = "footer">
+          
         </div>
         
       </header>
